ff41a313d156fa6331ef8da19e16b8e429b724d8
agriyp <agriyp@gmail.com>
2021-12-27 14:41:12 +0700

    menanmbahkan komen

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// mendeklarasikan var object
 const display1El = document.querySelector('.display-1');
 const display2El = document.querySelector('.display-2');
 const tempResultEl = document.querySelector('.temp-result');
